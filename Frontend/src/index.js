a3ef6e9bbc94dfce129f0989987c54eff193ce57
Ronal2 <ronal2.gutierrez@gmail.com>
2020-02-21 01:09:05 -0500

    backend con frontend

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,48 +1,9 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-import "./style.css";
+import App from './App'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
+const divHtml = document.getElementById('root')
 
-  async componentDidMount() {
-    const respuesta = await axios.get("http://localhost:8282/");
-    this.setState({ data: respuesta.data });
-  }
-
-  render() {
-    return this.state.data.map((curso, i) => {
-      return (
-        <div key={i} className="container">
-          <div>
-            <div className="container-data">
-              <div className="header">
-                <img
-                  src={"https://www.mytablemesa.com" + curso.imageUrl}
-                  className="img"
-                />
-                <p> {curso.maximumCredits + " CREDITS"} </p>
-              </div>
-              <div className="body">
-                <p className="title"> {curso.name} </p>
-                <p className="description"> {curso.imageText} </p>
-              </div>
-              <div className="footer">
-                <p className="price"> {"$" + curso.price} </p>
-                <p> {curso.rating} </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    });
-  }
-}
-
-export default App;
+render( <App/> ,
+    divHtml)
