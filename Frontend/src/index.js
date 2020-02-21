09d94bab100cf6fd1f8ff92dd3cf61b06442e934
Ronal2 <ronal2.gutierrez@gmail.com>
2020-02-21 00:56:53 -0500

    agregando frontend con backend

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./style.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get("http://localhost:8282/");
+    this.setState({ data: respuesta.data });
+  }
+
+  render() {
+    return this.state.data.map((curso, i) => {
+      return (
+        <div key={i} className="container">
+          <div>
+            <div className="container-data">
+              <div className="header">
+                <img
+                  src={"https://www.mytablemesa.com" + curso.imageUrl}
+                  className="img"
+                />
+                <p> {curso.maximumCredits + " CREDITS"} </p>
+              </div>
+              <div className="body">
+                <p className="title"> {curso.name} </p>
+                <p className="description"> {curso.imageText} </p>
+              </div>
+              <div className="footer">
+                <p className="price"> {"$" + curso.price} </p>
+                <p> {curso.rating} </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+}
+
+export default App;
